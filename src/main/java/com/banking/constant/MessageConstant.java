32eca5c63ee751c34cc5e545f82e9f78e2b7de62
Vu Hoai Nam <namhv2026@gmail.com>
2024-01-15 10:28:01 +0700

    up

--- src/main/java/com/banking/constant/MessageConstant.java
+++ src/main/java/com/banking/constant/MessageConstant.java
@@ -42,4 +42,6 @@ public class MessageConstant {
     public static final String LOGIN_FAILL ="user.login.fail";
 
     public static final String DELETE_SUSSCES = "delete sussces";
+
+    public static final String USER_DOB_INVALID = "user.dob.invalid";
 }
